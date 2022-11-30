461e4502a12027461a7d6d516e9eaf31e487723a
jmnesbitt <jmnesbitt@gmail.com>
2022-11-30 13:31:44 +0000

    Send all in

--- src/ExistInArray.java
+++ src/ExistInArray.java
@@ -5,7 +5,7 @@ public class ExistInArray {
     public static void main(String[] args) {
 
         //create and initialise and integer array, pre-fill with values of your choosing
-        int[] numbers = {2,4,6,8,10,12,14,16,18,20,22,24};
+        int[] numbers = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24};
 
         //ask the user to enter an integer value to search for in my array
         Scanner reader = new Scanner(System.in);
@@ -13,15 +13,20 @@ public class ExistInArray {
         //Output the user number
         int userNumber = reader.nextInt();
 
-        boolean found = IntStream.of(numbers).anyMatch(n -> n == userNumber);
+        int valueFind = 0;
 
-        /*Output
-If the value exists, output “Value X found in array” otherwise, output “Value was not found in array”.
-*/
-        if(found)
-            System.out.println("Value " + userNumber + " found in array");
-        else
-            System.out.println("Value was not found");
-
+        for (int i = 0; i < numbers.length; i++) {
+            if (userNumber == numbers[i]) {
+                valueFind++;
             }
         }
+            /*Output
+If the value exists, output “Value X found in array” otherwise, output “Value was not found in array”.
+*/
+        if (valueFind != 0) {
+            System.out.println("Value " + userNumber + " found in array");
+        } else
+            System.out.println("Value was not found");
+    }
+}
+
--- src/workbookQ2.java
+++ src/workbookQ2.java
@@ -1,15 +1,14 @@
 public class workbookQ2 {
     public static void main(String[] args) {
-        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        System.out.println(cars.length);
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};System.out.println(cars.length);
 
+        // call the element of the array
         System.out.println(cars[0]);
         System.out.println(cars[1]);
         System.out.println(cars[2]);
         System.out.println(cars[3]);
 
         // call all elements in the array using for loops
-
         for (int i=0; i<cars.length; i++){
             System.out.println(cars[i]);
         }
@@ -18,7 +17,7 @@ public class workbookQ2 {
         for (String x : cars) {
             System.out.println(x);
         }
-
+        // the size of the array using length
         System.out.println(cars.length); // question 3
     }
 
